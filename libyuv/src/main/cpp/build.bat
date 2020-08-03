3dec92042e243e5631e09cb89053dc1b51cd1b75
Madezhedoubeizhuce <Madezhedoubeizhuce@163.com>
2020-08-03 11:36:23 +0800

    1、添加gtest，使libyuv打开 TEST时编译通过 2、添加Windows编译脚本，方便Windows本地进行demo测试

---
+++ libyuv/src/main/cpp/build.bat
@@ -0,0 +1,21 @@
+echo off
+
+if "%1"=="clean" goto clean
+
+if not exist build (mkdir build)
+
+cd build
+
+REM set JPEG_LIBRARY=
+REM set JPEG_INCLUDE_DIR=
+
+cmake -G"MinGW Makefiles" -DTEST=ON ..
+cmake --build .
+make
+cd ..
+goto end
+
+:clean
+rmdir /s /q build
+
+:end
